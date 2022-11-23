b85097232ebfac0ddfdf2bb3a006f1588e0fcf71
bboybrian <tanszekai@gmail.com>
2022-11-23 18:37:22 +0000

    fix server

--- client/src/axios_config.js
+++ client/src/axios_config.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const backend = axios.create({
     withCredentials: false,
-    baseURL: "http://localhost:8080"
+    baseURL: "https://fluent-aileron-232404.ts.r.appspot.com/"
 });
 
 export default backend;
